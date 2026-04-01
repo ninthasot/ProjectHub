b4378d965d7fa9ed95377f835b0a5d0cb85d5ae2
Nikky <Nikky>
2026-04-02 00:04:57 +0200

    user repo interface

---
+++ src/ProjectHubBack/Modules/Identity/ProjectHub.Modules.Identity.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjectHub.Modules.Identity.Domain.Entities;
+
+namespace ProjectHub.Modules.Identity.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id, CancellationToken ct);
+    Task AddAsync(User user, CancellationToken ct);
+    Task UpdateAsync(User user, CancellationToken ct);
+    Task DeleteAsync(User user, CancellationToken ct);
+    Task<bool> ExistsAsync(string email, CancellationToken ct);
+    Task<User?> GetByEmailAsync(string email, CancellationToken ct);
+
+}
